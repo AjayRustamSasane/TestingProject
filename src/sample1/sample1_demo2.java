aa3677c48b266e960af6b8763e468421572a584b
Lenono <Lenono@DESKTOP-3HKEH2P>
2022-08-28 19:33:57 +0530

    [Ajay] GitHubProject new

---
+++ src/sample1/sample1_demo2.java
@@ -0,0 +1,14 @@
+package sample1;
+
+public class sample1_demo2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Good Morning");
+		
+		
+		
+		System.out.println("Good morning Ajay");
+		
+	}
+}
